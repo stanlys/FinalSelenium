104d3de6baeeda22e01123f5e8dde97aaf8c1f16
Stanlys <stanlys@yandex.ru>
2019-07-14 19:03:46 +0300

    Добавили "локаторы"

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,8 @@
 import time
 from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/"
-
+#link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 def go_to_login_page(browser):
     page = MainPage(browser,link)
     page.open()
