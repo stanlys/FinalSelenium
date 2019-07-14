3b065e15d1be900e924c80f072b3a124d0875f13
Stanlys <stanlys@yandex.ru>
2019-07-14 19:36:54 +0300

    Check forms login and registration

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
+import string
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,12 +10,14 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        url = self.browser.current_url
+        index = url.find('/login/')
+        assert index > 1, "This is not login page"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_REGISTER), "Register form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,17 @@
 import time
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
-#link = "http://selenium1py.pythonanywhere.com/"
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+link = "http://selenium1py.pythonanywhere.com/"
+#link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 def go_to_login_page(browser):
-    page = MainPage(browser,link)
+    page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_link()
+    #page.should_be_login_link()
+    login = LoginPage(browser ,link)
+    login.should_be_login_page()
+
 
 def test_guest_can_go_to_login_page(browser):
     browser.get(link)
